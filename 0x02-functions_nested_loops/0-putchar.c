a21d4735b44220b0d4c6219a025de9b91731f975
Martin Amankwaa <martyofmca@gmail.com>
2023-02-21 05:54:25 -0800

    Add task 0 source code file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Call the _putchar function
+ * to display the word putchar.
+ *
+ * Return: The success exit code 0
+ *
+ **/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+	return (0);
+}
