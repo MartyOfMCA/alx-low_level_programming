6fb6e013b1c6859e2c05d3170cbe29fad6442f94
Martin Amankwaa <martyofmca@gmail.com>
2023-02-21 21:40:40 -0800

    add task 10 source code file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers and return results
+ *
+ * @a: the first number
+ *
+ * @b: the second number
+ *
+ * Return: the sum of the two numbers
+ **/
+int add(int a, int b)
+{
+	return (a + b);
+}
