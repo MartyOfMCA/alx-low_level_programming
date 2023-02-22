74b39bf2931a587936d819cfb7798f04befd55a0
Martin Amankwaa <martyofmca@gmail.com>
2023-02-22 00:27:34 -0800

    mend

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,10 +27,9 @@ void print_to_98(int number)
 		{
 			printf("%d", number);
 			number++;
-			if (number < 98)
+			if (number <= 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				puts(", ");
 			}
 		}
 	}
