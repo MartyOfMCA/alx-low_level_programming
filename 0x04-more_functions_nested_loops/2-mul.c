c5341b1f467398d5bd08950510b12814ee92efac
Martin Amankwaa <martyofmca@gmail.com>
2023-02-23 09:58:56 -0800

    add task 2 source code file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Calculate the product for the
+ * given numbers.
+ *
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The product of the given numbers
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
