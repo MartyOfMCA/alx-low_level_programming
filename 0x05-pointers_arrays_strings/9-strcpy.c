dec73ddb85636ba09a918917bb235f8a86be710e
Martin Amankwaa <martyofmca@gmail.com>
2023-02-28 03:55:07 -0800

    add task 9 source code file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy the string specified as source to the
+ * specified destination.
+ *
+ * @src: The source string
+ * @dest: The destination string
+ *
+ * Return: A pointer to the destination string
+ *
+ **/
+char *_strcpy(char *dest, char *src)
+{
+	int index_of_src = 0;
+
+	while (src[index_of_src] != '\0')
+	{
+		dest[index_of_src] = src[index_of_src];
+		index_of_src++;
+	}
+	dest[index_of_src] = '\0';
+	return (dest);
+}
