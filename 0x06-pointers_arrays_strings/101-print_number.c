fb395280e09076c7a7d182f95965b960d482904c
Martin Amankwaa <martyofmca@gmail.com>
2023-03-04 02:26:57 -0800

    add advanced task 1 source code - 0x06

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_number - Display the given integer.
+ *
+ * @n: The number to be displayed
+ **/
+void print_number(int n)
+{
+	int first = n, count = 0, x = 1, i, tmp;
+
+	tmp = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (n * -1) - 1;
+	}
+	while (first != 0)
+	{
+		first = first / 10;
+		count++;
+	}
+	while (x <= count)
+	{
+		first = n;
+		i = x;
+		while (i < count)
+		{
+			first = first / 10;
+			i++;
+		}
+		if (tmp < 0 && x == count)
+			_putchar(((first % 10) + 48) + 1);
+		else
+			_putchar((first % 10) + 48);
+		x++;
+	}
+	if (count == 0)
+		_putchar('0');
+}
