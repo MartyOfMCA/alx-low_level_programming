a65d1a65ade1de15779e62bba2b5b07142e7040f
Martin Amankwaa <martyofmca@gmail.com>
2023-03-06 18:27:28 -0800

    add task 5 source code file - 0x07

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strstr - Finds the first occurence of the substring
+ * @needle in @haystack.
+ *
+ * @haystack: The string to be searched
+ * @needle: The string to search for
+ *
+ * Return: A pointer to the memory address
+ * which begins the located substring or NULL.
+ *
+ **/
+char *_strstr(char *haystack, char *needle)
+{
+	int count = 0, j = 0, ii = 0, s = 0;
+
+	if (needle[ii] == '\0')
+	{
+		return (&haystack[ii]);
+	}
+
+	for ( ; needle[s]; s++)
+	{
+	}
+
+	for ( ; needle[ii]; ii++)
+	{
+		j = ii;
+		for ( ; haystack[j]; j++)
+		{
+			if (needle[ii] == haystack[j])
+			{
+				count++;
+				j++;
+				break;
+			}
+		}
+	}
+	if (count == s)
+	{
+		return (&haystack[j - s]);
+	}
+	return (NULL);
+}
