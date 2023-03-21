095727144a18fe4c8d4579b8adaf717b568cc8a2
Martin Amankwaa <martyofmca@gmail.com>
2023-03-20 19:13:55 -0700

    add a new type for the dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,6 +24,7 @@ struct dog
  * dog_t - Typedef for dog record
  */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
