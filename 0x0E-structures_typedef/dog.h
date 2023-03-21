5d9f1f59dd88d3803bc7f42d31de83c62ea1a95b
Martin Amankwaa <martyofmca@gmail.com>
2023-03-20 17:31:33 -0700

    add header file with dog record - 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A record on a dog.
+ *
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: This stores the record
+ * of a dog.
+ *
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
