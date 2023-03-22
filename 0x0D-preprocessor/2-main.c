6099021a3d45ee65ea397b65347210ffecef596a
Martin Amankwaa <martyofmca@gmail.com>
2023-03-21 17:30:45 -0700

    add file to print the name of the file compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the name of this file.
+ *
+ * Return: The success exit code
+ *
+ **/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
