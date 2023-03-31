08ba1c687cc8744cc6296639e54c5a71e46d7feb
Martin Amankwaa <martyofmca@gmail.com>
2023-03-30 22:01:30 -0700

    add constructor for main program

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * constructor - A constrcuor function for main
+ * program
+ *
+ **/
+void constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
