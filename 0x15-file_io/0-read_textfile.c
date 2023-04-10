b282dff21be9577d62137e09c0b7e42341735e65
Martin Amankwaa <martyofmca@gmail.com>
2023-04-10 11:04:37 -0700

    read contents from file to std out

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads the contents from the specified
+ * text file.
+ *
+ * @filename: The file to be read from
+ * @letters: The characters to be read from the file
+ *
+ * Return: The number of characters read from file
+ *
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor, chars_read, chars_written;
+	char *str;
+
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL || filename == NULL)
+	{
+		return (0);
+	}
+	file_descriptor = open(filename, O_RDONLY, 0500);
+	if (file_descriptor == -1)
+	{
+		return (0);
+	}
+	chars_read = read(file_descriptor, str, letters);
+	if (chars_read == -1)
+	{
+		return (0);
+	}
+	chars_written = write(STDOUT_FILENO, str, chars_read);
+	if (chars_written == -1 || chars_written != (int)letters)
+	{
+		return (0);
+	}
+	return (chars_read);
+}
