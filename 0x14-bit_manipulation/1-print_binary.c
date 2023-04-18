c48159c7711b859132142c5b2c3aded6553baef8
Martin Amankwaa <martyofmca@gmail.com>
2023-04-18 16:22:11 -0700

    use bit masking to display binary for a decimal

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,16 +10,17 @@
  **/
 void print_binary(unsigned long int n)
 {
-	unsigned int bit_val, shift_val = n;
-
-	if (n == 0)
+	/* Move to the least significant bit */
+	if (n >> 1 > 0)
 	{
-		_putchar('0');
+		print_binary(n >> 1);
 	}
-	while (shift_val > 0)
-	{
-		bit_val = (n & shift_val);
-		(bit_val == 0) ? _putchar('0') : _putchar('1');
-		shift_val >>= 1;
-	}
+	/*
+	 * Print bit value in the reverse order.
+	 * When the least significant bit in each
+	 * recursive call is 1 then the bitwise &
+	 * of 1 would print the value 1 to indicate
+	 * the bit is turned on
+	 */
+	_putchar((n & 1) + 48);
 }
