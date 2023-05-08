acaea1e3c819d9feab2e4d695dfccc5c1451b4c7
MArtin Amankwaa <martyofmca@gmail.com>
2023-05-08 15:47:54 +0100

    create a file with some contents - 0x15

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,8 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-int _putchar(char character);
+int _putchar(char c);
+int _strlen(char *str);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
