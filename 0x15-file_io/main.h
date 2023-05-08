f19903b51f8b6ec0c3fd53572b4741ea6475010d
MArtin Amankwaa <martyofmca@gmail.com>
2023-05-08 15:54:05 +0100

    copy contents from one file to another file - 0x15

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void from_error(char *str);
+void to_error(char *str);
+void close_error(int file_descriptor);
 
 #endif
